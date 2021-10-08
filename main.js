5025c9078d9c48aabbc13bffbb74bd0260e7c86f
SucioLoco <irelandrj@yahoo.com>
2021-10-08 13:58:44 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// let clear = document.getElementById("clear")
+const button = document.getElementsByClassName("button")
+
+var div = document.getElementsByClassName('button')[0];
+
+ div.addEventListener('click', function () {
+     alert('Hi!');
+ });
